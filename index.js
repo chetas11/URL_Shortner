ff5ffc5419239a99cbeec4eadbf9e962f7dfff27
chetas11 <cbirmole@gmail.com>
2020-12-01 17:07:35 +0530

    added random string on mail

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const MongoClient = require('mongodb').MongoClient;
 require("dotenv").config();
 
 const url = process.env.MONGO_URL;
+const password = process.env.MAILPASSWORD;
+
+var nodemailer = require('nodemailer'); 
+var randomstring = require("randomstring");
+
 
 
 // MongoClient.connect(url, function(err, db) {
@@ -26,22 +31,41 @@ app
     res.sendFile(__dirname +"/index.html")  
 })
 .post("/reset",(req, res)=>{
-    res.sendFile(__dirname+"/public/Failed.html")
-    MongoClient.connect(url || process.env.MONGODB_URI, function(err, db) {
+    MongoClient.connect(url || process.env.MONGODB_URI, { useUnifiedTopology: true }, function(err, db) {
     if (err) throw err;
     let dbo = db.db("trail");
     let query = { email: req.body.email };
     dbo.collection("AllData").find(query).toArray(function(err, result) {
         if (err) throw err;
-        if(result.length>0){
-            console.log("User Found") 
-        }
         if(result.length===0){
-            console.log("User Not Found")
+            res.sendFile(__dirname+"/public/Failed.html")
         }
+        else{
+            res.sendFile(__dirname+"/public/success.html")
+            var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'mangeshvalanju13@gmail.com',
+                pass:  password
+            }
+            });
+            var mailOptions = {
+            from: 'mangeshvalanju13@gmail.com',
+            to: req.body.email,
+            subject: 'Reset Password for your account',
+            text: randomstring.generate()
+            };
+        }
+
+        transporter.sendMail(mailOptions, function(error, info){
+        if (error) console.log(error);}); 
         db.close();
     });
     });
+})
+
+.post("/update",(req, res)=>{
+
 })
 
 .listen(8000);
