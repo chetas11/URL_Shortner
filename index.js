912128b6c139dc6b0a0a5b39505499ac6b76606d
chetas11 <cbirmole@gmail.com>
2020-12-01 21:58:17 +0530

    added password reset form

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser")
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 require("dotenv").config();
+var sessionstorage = require('sessionstorage');
+var md5 = require('md5');
 
 const url = process.env.MONGO_URL;
 const password = process.env.MAILPASSWORD;
@@ -31,6 +33,7 @@ app
     res.sendFile(__dirname +"/index.html")  
 })
 .post("/reset",(req, res)=>{
+    let random = randomstring.generate();
     MongoClient.connect(url || process.env.MONGODB_URI, { useUnifiedTopology: true }, function(err, db) {
     if (err) throw err;
     let dbo = db.db("trail");
@@ -53,20 +56,73 @@ app
             from: 'mangeshvalanju13@gmail.com',
             to: req.body.email,
             subject: 'Reset Password for your account',
-            text: randomstring.generate()
+            text: "Please enter this string to procceed with resetting of passowrd: "+ random
             };
         }
-
-        transporter.sendMail(mailOptions, function(error, info){
-        if (error) console.log(error);}); 
+        // transporter.sendMail(mailOptions, function(error, info){
+        // if (error) console.log(error);}); 
         db.close();
     });
     });
+
+    sessionstorage.setItem("randomStr", random)
+
+    MongoClient.connect(url || process.env.MONGODB_URI, { useUnifiedTopology: true }, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("trail");
+            var myquery = { email: req.body.email };
+            var newvalues = { $set: {tempString : random } };
+            dbo.collection("AllData").updateOne(myquery, newvalues, function(err, res) {
+                if (err) throw err;
+                db.close();
+            });
+        });
 })
 
-.post("/update",(req, res)=>{
-
+.post("/verify",(req, res)=>{
+   MongoClient.connect(url || process.env.MONGODB_URI, { useUnifiedTopology: true }, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("trail");
+            var query = { tempString : req.body.randomString };
+            dbo.collection("AllData").find(query).toArray(function(err, result) {
+                if(result.length > 0){
+                    res.sendFile(__dirname+"/public/reset.html")
+                }else{
+                    res.end("Incorrect")
+                }
+                db.close();
+            });
+        });
 })
+
+.post("/changepassword",(req, res)=>{
+   if(req.body.Password === req.body.ConfirmPassword){
+        MongoClient.connect(url || process.env.MONGODB_URI, { useUnifiedTopology: true }, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("trail");
+            var myquery = { tempString: sessionstorage.getItem("randomStr") };
+            var newvalues = { $set: {password : md5(req.body.Password) } };
+            dbo.collection("AllData").updateOne(myquery, newvalues, function(err, res) {
+                if (err) throw err;
+            });
+        });
+        MongoClient.connect(url || process.env.MONGODB_URI, { useUnifiedTopology: true }, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("trail");
+            var myquery = { tempString: sessionstorage.getItem("randomStr") };
+            var newvalues = { $set: {tempString : "" } };
+            dbo.collection("AllData").updateOne(myquery, newvalues, function(err, res) {
+                if (err) throw err;
+                db.close();
+            });
+        });
+   }else{
+       console.log("Password not matched")
+   }
+   
+})
+
+
 
 .listen(8000);
 
